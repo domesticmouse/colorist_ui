dbf7d75ba1e1786d415941ddfa3d1af54eb7224e
Brett Morgan <brett.morgan@gmail.com>
2026-08-19 17:57:31 +1000

    Update dependencies

--- lib/src/models/chat_state.freezed.dart
+++ lib/src/models/chat_state.freezed.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // coverage:ignore-file
-// ignore_for_file: type=lint
+// ignore_for_file: type=lint, type=warning, deprecated_member_use, deprecated_member_use_from_same_package
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'chat_state.dart';
@@ -9,6 +9,7 @@ part of 'chat_state.dart';
 // FreezedGenerator
 // **************************************************************************
 
+// GENERATED CODE - DO NOT MODIFY BY HAND
 // dart format off
 T _$identity<T>(T value) => value;
 
@@ -66,7 +67,7 @@ class _$ChatStateCopyWithImpl<$Res>
 /// Create a copy of ChatState
 /// with the given fields replaced by the non-null parameter values.
 @pragma('vm:prefer-inline') @override $Res call({Object? messages = null,}) {
-  return _then(_self.copyWith(
+  return _then(ChatState(
 messages: null == messages ? _self.messages : messages // ignore: cast_nullable_to_non_nullable
 as List<Message>,
   ));
@@ -209,7 +210,7 @@ return $default(_that.messages);case _:
 @JsonSerializable()
 
 class _ChatState extends ChatState {
-  const _ChatState({required final  List<Message> messages}): _messages = messages,super._();
+  const _ChatState({required  List<Message> messages}): _messages = messages,super._();
   factory _ChatState.fromJson(Map<String, dynamic> json) => _$ChatStateFromJson(json);
 
  final  List<Message> _messages;
--- lib/src/models/log_state.freezed.dart
+++ lib/src/models/log_state.freezed.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // coverage:ignore-file
-// ignore_for_file: type=lint
+// ignore_for_file: type=lint, type=warning, deprecated_member_use, deprecated_member_use_from_same_package
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'log_state.dart';
@@ -9,6 +9,7 @@ part of 'log_state.dart';
 // FreezedGenerator
 // **************************************************************************
 
+// GENERATED CODE - DO NOT MODIFY BY HAND
 // dart format off
 T _$identity<T>(T value) => value;
 
@@ -66,7 +67,7 @@ class _$LogStateCopyWithImpl<$Res>
 /// Create a copy of LogState
 /// with the given fields replaced by the non-null parameter values.
 @pragma('vm:prefer-inline') @override $Res call({Object? logEntries = null,}) {
-  return _then(_self.copyWith(
+  return _then(LogState(
 logEntries: null == logEntries ? _self.logEntries : logEntries // ignore: cast_nullable_to_non_nullable
 as List<LogEntry>,
   ));
@@ -209,7 +210,7 @@ return $default(_that.logEntries);case _:
 @JsonSerializable()
 
 class _LogState extends LogState {
-  const _LogState({required final  List<LogEntry> logEntries}): _logEntries = logEntries,super._();
+  const _LogState({required  List<LogEntry> logEntries}): _logEntries = logEntries,super._();
   factory _LogState.fromJson(Map<String, dynamic> json) => _$LogStateFromJson(json);
 
  final  List<LogEntry> _logEntries;
